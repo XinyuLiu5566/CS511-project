e69bf92164011983acfff07ba090b3326ec9994d
liuxinyu <xinyu6@illinois.edu>
2021-10-17 17:21:42 -0500

    sql model

--- cs511project/cs511project/settings.py
+++ cs511project/cs511project/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'cs511project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'root',
+        'PASSWORD': '00000000',
+
     }
 }
 
