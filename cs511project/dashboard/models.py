a15e44b35d0459a475c5df2788b6c580b80486c1
joseph1027 <joseph102712080104@gmail.com>
2021-10-17 01:50:13 -0500

    read table content from MySQL database

--- cs511project/dashboard/models.py
+++ cs511project/dashboard/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Phone(models.Model):
+	app_name = models.CharField(max_length=100)
+	app_id = models.CharField(max_length=100)
+	category = models.CharField(max_length=100)
+	class Meta:
+		db_table = "google_app_store"
--- cs511project/dashboard/views.py
+++ cs511project/dashboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Phone
 
-# Create your views here.
+def home(request):
+	phones = Phone.objects.all()
+	context = {'phones': phones}
+	return render(request, 'dashboard/home.html', context)
+
+def show(request):
+	return HttpResponse('<h1> Welcome show! </h1>')
